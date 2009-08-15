a8715e95b44a8d369d93a5c724469f3f5a24d1bf
Chris Flipse <cflipse@gmail.com>
2009-08-15 01:11:54 -0400

    pull drb port value from RSPEC_DRB environment

--- lib/spec/runner/drb_command_line.rb
+++ lib/spec/runner/drb_command_line.rb
@@ -4,13 +4,19 @@ module Spec
   module Runner
     # Facade to run specs by connecting to a DRB server
     class DrbCommandLine
+      # port to run against
+      def self.port
+        (ENV["RSPEC_DRB"] || 8989).to_i
+      end
+
+
       # Runs specs on a DRB server. Note that this API is similar to that of
       # CommandLine - making it possible for clients to use both interchangeably.
       def self.run(options)
         begin
           # See http://redmine.ruby-lang.org/issues/show/496 as to why we specify localhost:0
           DRb.start_service("druby://localhost:0")
-          spec_server = DRbObject.new_with_uri("druby://127.0.0.1:8989")
+          spec_server = DRbObject.new_with_uri("druby://127.0.0.1:#{port}")
           spec_server.run(options.argv, options.error_stream, options.output_stream)
           true
         rescue DRb::DRbConnError
@@ -18,6 +24,7 @@ module Spec
           false
         end
       end
+
     end
   end
 end
